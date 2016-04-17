d71f4bf4a4dd1fa3eeb7559c5b181e2d082ad35b
chandler <771690057@qq.com>
2016-04-17 15:53:07 +0800

    加入vue-router 及 transition 功能

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <img class="logo" src="./assets/logo.png">
     <hello></hello>
     <p>
@@ -15,9 +15,7 @@
       <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
     </p>
     <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
+      <a v-link="'/Page1'">go to page1</a>
     </p>
   </div>
 </template>
@@ -33,32 +31,5 @@ export default {
 </script>
 
 <style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+  @import "assets/common.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter)
+
+var myApp = Vue.extend({
+  el () { return 'body' },
+  data () {
+    return { transition: 'slide-left' }
+  }
 })
+var myRouter = new VueRouter()
+
+myRouter.map({'/': {
+  name: 'home',
+  component: App
+}})
+
+myRouter.map({'/Page1': {
+  name: 'page1',
+  component: function (resolve) {
+    resolve(require('components/Page1.vue'))
+  }
+}})
+
+myRouter.beforeEach(function (trans) {
+  // 拦截地址, 如果是返回, 则改变动画方向
+  if (trans.to.query.back && Boolean(trans.to.query.back) === true) {
+    myRouter.app.transition = 'slide-right'
+  } else {
+    myRouter.app.transition = 'slide-left'
+  }
+  trans.next()
+})
+
+myRouter.start(myApp, 'body')
